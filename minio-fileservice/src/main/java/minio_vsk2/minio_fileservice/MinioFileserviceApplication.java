27d994642d43dfa39e97eb54bf09ef29bf7ebd0c
Alibibib <abdilovalibi@gmail.com>
2025-12-05 15:55:31 +0500

    first commit

---
+++ minio-fileservice/src/main/java/minio_vsk2/minio_fileservice/MinioFileserviceApplication.java
@@ -0,0 +1,13 @@
+package minio_vsk2.minio_fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioFileserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinioFileserviceApplication.class, args);
+	}
+
+}
---
+++ minio-fileservice/src/test/java/minio_vsk2/minio_fileservice/MinioFileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package minio_vsk2.minio_fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioFileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
